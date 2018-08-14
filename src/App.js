5828f2ff451088172dabf26d74dde749d03680d8
Brian Burns <bburns.km@gmail.com>
2018-08-14 12:01:22 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
+// yr
+
 import React from 'react';
 import Terminal from 'terminal-in-react';
+import './App.css';
+
 
 const welcomeMessage = "Welcome to the dark forest. An ancient path leads onwards...";
 
 const commands = {
-  // 'open-google': () => window.open('https://www.google.com/', '_blank'),
-  // showmsg: this.showMsg,
-  // popup: () => alert('Terminal in React'),
   glomp: () => alert("glomp glomp glomp!"),
+  quirp: () => alert("quirp!"),
+  // 'open-google': () => window.open('https://www.google.com/', '_blank'),
 };
 
 const descriptions = {
-  // 'open-google': 'opens google.com',
-  // showmsg: 'shows a message',
-  // alert: 'alert', popup: 'alert',
   glomp: "glomp-attack!",
+  quirp: "a blue mushroom",
+  // 'open-google': 'opens google.com',
+  // alert: 'alert',
 };
 
 
 class App extends React.Component {
 
-  // showMsg = () => 'Hello World';
-
+  // style={{ fontWeight: "bold", fontSize: "1em" }}
   render() {
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh"
-        }}
-      >
+      <div className="container">
         <Terminal
           color='green'
           backgroundColor='black'
           barColor='black'
-          style={{ fontWeight: "bold", fontSize: "1em" }}
           commands={commands}
           descriptions={descriptions}
           msg={welcomeMessage}
