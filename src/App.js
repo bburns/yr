34735ec792891f2c403488d9187ad67e15605e5d
Brian Burns <bburns.km@gmail.com>
2018-08-14 11:57:04 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Terminal from 'terminal-in-react';
 
+const welcomeMessage = "Welcome to the dark forest. An ancient path leads onwards...";
+
+const commands = {
+  // 'open-google': () => window.open('https://www.google.com/', '_blank'),
+  // showmsg: this.showMsg,
+  // popup: () => alert('Terminal in React'),
+  glomp: () => alert("glomp glomp glomp!"),
+};
+
+const descriptions = {
+  // 'open-google': 'opens google.com',
+  // showmsg: 'shows a message',
+  // alert: 'alert', popup: 'alert',
+  glomp: "glomp-attack!",
+};
+
 
 class App extends React.Component {
 
-  showMsg = () => 'Hello World';
+  // showMsg = () => 'Hello World';
 
   render() {
     return (
@@ -21,19 +37,9 @@ class App extends React.Component {
           backgroundColor='black'
           barColor='black'
           style={{ fontWeight: "bold", fontSize: "1em" }}
-          commands={{
-            // 'open-google': () => window.open('https://www.google.com/', '_blank'),
-            // showmsg: this.showMsg,
-            // popup: () => alert('Terminal in React'),
-            glomp: () => alert("glomp glomp glomp!"),
-          }}
-          descriptions={{
-            // 'open-google': 'opens google.com',
-            // showmsg: 'shows a message',
-            // alert: 'alert', popup: 'alert',
-            glomp: "glomp-attack!",
-          }}
-          msg="Welcome to the dark forest. An ancient path leads onwards..."
+          commands={commands}
+          descriptions={descriptions}
+          msg={welcomeMessage}
         />
       </div>
     );
